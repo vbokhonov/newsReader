f28d7613f6042633ed10c4f4f37efef7cbab2faa
Valentyn Bokhonov <v.bokhonov@serverpipe.com>
2021-06-01 17:41:30 +0300

    added: ico, load screen, xib cell

---
+++ newsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsReader
+//
+//  Created by Valentyn Bokhonov on 01.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
